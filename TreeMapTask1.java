caf9091d62f491f4ebe18da5a10cc1cdb8ff9619
mahathi-7128 <mahathireddygala@gmail.com>
2025-06-05 14:46:48 +0530

    Add files via upload

---
+++ TreeMapTask1.java
@@ -0,0 +1,22 @@
+package xyz;
+
+import java.util.TreeMap;
+
+public class TreeMapTask1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      TreeMap<String, String> country=new TreeMap<>();
+      country.put("india", "newdelhi");
+      country.put("usa", "washington");
+      country.put("germany", "berlin");
+      country.put("japan", "tokyo");
+      country.put("india", "delhi");
+      System.out.println(country);
+      System.out.println(country.firstEntry());
+      System.out.println(country.lastEntry());
+      System.out.println(country.higherEntry("germany"));
+      System.out.println(country.lowerEntry("india"));
+	}
+
+}
